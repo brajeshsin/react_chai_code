5fc6e7dd57e17ed6936d631e454c56c103f201c8
brajeshsin <brajeshsingh2911@gmail.com>
2025-05-22 12:13:31 +0530

    babel use

--- 04react/src/App3.js
+++ 04react/src/App3.js
@@ -1,5 +1,5 @@
-import React from "https://esm.sh/react@19.1.0";
-import ReactDOM from "https://esm.sh/react-dom@19.1.0/client";
+// import React from "https://esm.sh/react@19.1.0";
+// import ReactDOM from "https://esm.sh/react-dom@19.1.0/client";
 
 const Chai = (props) => {
   console.log(props);
