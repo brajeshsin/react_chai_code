ae51a64188951568689a1c494ddaef42a76a3140
brajeshsin <brajeshsingh2911@gmail.com>
2025-05-22 22:11:45 +0530

    Web request

---
+++ 05react/src/App.jsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to my code....</h1>
+      <p>Learning react from scratch..</p>
+    </div>
+  );
+}
---
+++ 06react/05react/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data, ">>>>>>>>");
+        setMessage(data);
+      })
+      .catch(() => setMessage("failed"));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome to my code....</h1>
+      <p>Learning react from scratch..</p>
+
+      {message.map((mydata) => (
+        <h2 key={mydata.id}>{mydata.title}</h2>
+      ))}
+    </div>
+  );
+}
