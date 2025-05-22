18e5485638590c8092ffa97f96b2c656680b0053
brajeshsin <brajeshsingh2911@gmail.com>
2025-05-22 22:14:14 +0530

    Web request

--- 06react/05react/src/App.jsx
+++ 06react/05react/src/App.jsx
@@ -10,7 +10,7 @@ export function App() {
         console.log(data, ">>>>>>>>");
         setMessage(data);
       })
-      .catch(() => setMessage("failed"));
+      .catch(() => setMessage("faileddd"));
   }, []);
 
   return (
